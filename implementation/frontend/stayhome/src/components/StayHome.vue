2bd41bb933080959145ccd82a761bad0408694c0
Fgarcia19 <40769836+Fgarcia19@users.noreply.github.com>
2020-06-26 01:31:13 -0500

    Update StayHome.vue

--- implementation/frontend/stayhome/src/components/StayHome.vue
+++ implementation/frontend/stayhome/src/components/StayHome.vue
@@ -1,216 +1,263 @@
 <template>
-    <v-container>
-      <v-row>  
-      <h1>{{currentUser.username}} 
-       <v-text class= "admin" > >> Administrar Perfil </v-text> </h1>
-      </v-row>
-      <v-spacer></v-spacer>
+  <v-container>
+      <v-container class="grey lighten-3">
+        <v-row class="text-center" >
+          <v-col
+            v-for="(item,n) in items"
+            :key="n"
+            cols="100"
+            sm="4"
+          >
+            <v-card
+              class="titulo"
+              outlined
+              tile
+              align="center" justify="center"
+            >
+              {{item.title}} 
+            </v-card>
+            <v-row  >
+          <v-col
+            v-for="(item2,i) in items2"
+            :key="i"
+            cols="100"
+            sm="4"
+
+          >
+          <v-bottom-navigation
+                v-model="bottomNav"
+                dark
+            >
+
+            <v-btn
+              class="white--text"
+              tile
+              dark
+              center
+            >
+            <span>{{item2.title}}</span>
+            <v-icon > {{item2.icon}} </v-icon>
+
+            </v-btn>
+            </v-bottom-navigation> 
+          </v-col>
+        </v-row>
+          </v-col>
+        </v-row>
+      </v-container>
+     
       <v-container>
-        <h2 class="text-md-center" > ¿Qué desea editar?</h2>
-
-        <v-row class="pa-3" justify="center">
-          Username: {{currentUser.username}}
-          <v-dialog v-model="dialog1" persistent max-width="290"> 
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn style="margin-left: 40px" class="subheading"
-                  small 
-                  dark
-                  v-bind="attrs"
-                  v-on="on"
-                  color="dark"
-                  ><v-icon left>mdi-pencil</v-icon>Editar
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title class="headline">Cambiar username</v-card-title>
-                <v-card-text> 
-                  <v-row>
-                    <v-col cols="12"> 
-                      <v-text-field v-model="editInfo.username" label="Username" :rules="nameRules"/>
-                    </v-col>
-                  </v-row>  
-                </v-card-text>
-                <v-card-actions>
-                  <v-spacer></v-spacer>
-                  <v-btn color="green darken-1" text @click="dialog1 = false">Cerrar</v-btn>
-                  <v-btn color="green darken-1" text @click="userUpdate">Guardar cambios</v-btn>
-                </v-card-actions>
-              </v-card>
-          </v-dialog>
-        </v-row>
-      
-        <v-row class="pa-3" justify="center">
-          Email: {{currentUser.email}}
-          <v-dialog v-model="dialog2" persistent max-width="290"> 
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn style="margin-left: 40px" class="subheading"
-                  small  
-                  dark
-                  v-bind="attrs"
-                  v-on="on"
-                  color="dark"
-                  ><v-icon left>mdi-pencil</v-icon>Editar
-                </v-btn>
-              </template>
-              <v-card>
-                <v-form v-model="valid">
-                  <v-card-title class="headline">Cambiar email</v-card-title>
-                  <v-card-text>
-                    <v-row>
-                      <v-col cols="12"> 
-                          <v-text-field v-model="editInfo.email" label="Email" :rules="emailRules"/>
-                      </v-col>
-                    </v-row>             
-                  </v-card-text>
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="green darken-1" text @click="dialog2 = false">Cerrar</v-btn>
-                    <v-btn color="green darken-1" text @click="userUpdate" :disabled="!valid">Guardar cambios</v-btn>
-                  </v-card-actions>
-                </v-form>
-              </v-card>
-          </v-dialog>
-        </v-row>
-      
-        <v-row class="pa-3" justify="center">
-          Password: {{currentUser.password}}
-          <v-dialog v-model="dialog3" persistent max-width="290"> 
-              <template v-slot:activator="{ on, attrs }">
-                <v-btn style="margin-left: 40px" class="subheading"
-                  small  
-                  dark
-                  v-bind="attrs"
-                  v-on="on"
-                  color="dark"
-                  ><v-icon left>mdi-pencil</v-icon>Editar
-                </v-btn>
-              </template>
-              <v-card>
-                <v-card-title class="headline">Cambiar contraseña</v-card-title>
-                <v-card-text> 
-                <v-row>
-                  <v-col cols="12"> 
-                      <v-text-field v-model="editInfo.password" 
-                      label="Password" 
-                      :type="showPassword ? 'text' : 'password' "
-                      :append-icon="showPassword ? 'mdi-eye' : 'mdi-eye-off'"
-                      :rules="passwordRules"
-                      @click:append="showPassword=!showPassword"/>
-                  </v-col>
-                </v-row>  
-                </v-card-text>
-              <v-card-actions>
-              <v-spacer></v-spacer>
-                <v-btn color="green darken-1" text @click="dialog3 = false">Cerrar</v-btn>
-                <v-btn color="green darken-1" text @click="userUpdate">Guardar cambios</v-btn>
-              </v-card-actions>
-              </v-card>
-          </v-dialog> 
-
-        </v-row>
-    
-        <v-row class="pa-5" justify="center" >
-          <v-btn  small  @click="userDelete"  color="error" >Eliminar cuenta</v-btn>
-        </v-row>
-
-        <v-row  class="pa-9" align="center" justify="center">
-          <v-img src="../../public/img/edit2.png"
-            max-width="400"
-            max-height="400"
+       
+    <v-card flat tile>
+    <v-window v-model="onboarding" vertical >
+      <v-window-item
+        v-for="n in length"
+        :key="`card-${n}`"
+        
+      >
+        <v-card
+          color="grey"
+          height="200"
+          width="340"
+        >
+          <v-row
+            class="fill-height"
+            align="center"
             justify="center"
-          ></v-img>
-        </v-row>
+            tag="v-card-text"
+          >
+            <h1 style="font-size: 5rem;" class="white--text">Artículo {{ n }}</h1>
+          </v-row>
+        </v-card>
+      </v-window-item>
+    </v-window>
 
-    </v-container>
+    <v-card-actions class="justify-left">
+      <v-btn
+        text
+        @click="prev"
+        
+      >
+        <v-icon>mdi-chevron-up</v-icon>
+      </v-btn>
+      <v-item-group
+        v-model="onboarding"
+        class="text-center"
+        mandatory
+      >
+     
+      </v-item-group>
+      <v-btn
+        text
+        @click="next"
+      >
+        <v-icon>mdi-chevron-down</v-icon>
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+      </v-container>
+      <v-container style="center" class="text-md-center" justify="center">
+         <v-btn
+          :loading="loading3"
+          :disabled="loading3"
+          color="blue-grey"
+          class="ma-2 white--text"
+          @click="loader = 'loading3'"
+          
+         
+        >
+          Upload
+        <v-icon right dark>mdi-cloud-upload</v-icon>
+        </v-btn>
 
-  </v-container>
+
+
+      </v-container>
+
+</v-container>
 </template>
 
 <script>
 import { mapState } from 'vuex';
 export default {
-  name: 'Update',
-  computed: {
-    ...mapState(['currentUser'])
-  },
-  data() {
-    return {
-      showPassword : false,
-      editInfo:{
-        user_id: '',
-        username: '',
-        email: '',
-        password: ''
-       },
-      emailRules: [
-        v => !!v || 'Email requerido',
-        v => /.+@.+\..+/.test(v) || 'Email tiene que ser valido',
-      ],
-      nameRules: [
-        v => !!v || 'Username requirido',
-        v => (v && v.length <= 15) || 'Username tiene que ser menor que 15 caracteres',
-      ],
-      passwordRules: [
-        v => !!v || 'Password requirido',
-        v => (v && v.length <= 20) || 'Password tiene que ser menor que 20 caracteres',
-      ],
-      dialog1: false,
-      dialog2: false,
-      dialog3: false
-    }
-  },
-  mounted(){
-    this.$store.dispatch("loadAll")
-  },
-  methods: {
-    async userUpdate(){
 
-        if(this.editInfo.password!='')
-        {
-          this.editInfo.username = this.currentUser.username;
-          this.editInfo.email = this.currentUser.email;
-        }
-        else if(this.editInfo.email!=='')
-        {
-          this.editInfo.username = this.currentUser.username;
-          this.editInfo.password = this.currentUser.password;
-        }
-        else
-        {
-          this.editInfo.password = this.currentUser.password;
-          this.editInfo.email = this.currentUser.email;
-        }
+    
+    
+    name: 'StayHome',
+    data(){
+        return{
 
-        this.editInfo.user_id = this.currentUser.user_id;
-        let user = await this.$store.dispatch('updateUser', this.editInfo);
-          if(user.error){
-            alert(user.error)
-          }else{
-            alert('Se han actualizado tus datos con éxito');
+          length: 3,
+          onboarding: 0,
+
+           loader: null,
+           loading3: false,
+
+            bottomNav: 2,
+            
+             items: [
+          {
+            title: 'Matemática',
+          },
+          {
+            title: 'Física',
+          },
+          {
+            title: 'Química',
           }
-        this.$router.push("/mainpage")
-      },
-      userDelete()
-      {
-        let response = confirm(`¿Estás seguro que deseas eliminar tu cuenta? ${this.currentUser.username}`);
-        if(response)
-        {
-          this.$store.dispatch('deleteUser',this.currentUser);
-          console.log(this.currentUser);
-          this.$router.push("/");
+            ],
+
+             items2: [
+          {
+            title: 'Videos', icon: 'mdi-television-play'
+          },
+          {
+            title: 'Docs', icon: 'mdi-book'
+          },
+          {
+            title: 'Imágenes', icon: 'mdi-image'
+          }
+            ]
         }
-      }
-    }
-  
+    },
+    computed: {
+
+        ...mapState(['currentUser'])
+
+    },
+    mounted(){
+    this.$store.dispatch("loadAll")
+    },
+    methods:
+    {
+        admin()
+        {
+                this.$router.push("/update")
+        },
+
+      next () {
+        this.onboarding = this.onboarding + 1 === this.length
+          ? 0
+          : this.onboarding + 1
+      },
+      prev () {
+        this.onboarding = this.onboarding - 1 < 0
+          ? this.length - 1
+          : this.onboarding - 1
+      },
+
+
+
+
+
+    },
+
+    watch: {
+      loader () {
+        const l = this.loader
+        this[l] = !this[l]
+
+        setTimeout(() => (this[l] = false), 3000)
+
+        this.loader = null
+      },
+    },
 }
+       
 </script>
 
 <style lang="scss" scoped>
 
-.admin{
-        
-        font-style: Arial;
-        font-size: 18px;
+  .custom-loader {
+    animation: loader 1s infinite;
+    display: flex;
+  }
+  @-moz-keyframes loader {
+    from {
+      transform: rotate(0);
     }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-webkit-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @-o-keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+  @keyframes loader {
+    from {
+      transform: rotate(0);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+
+.titulo{
+        text-align: center;
+        font-style: normal;
+        font-size: 27px;
+        letter-spacing: 0.8px;
+        word-spacing: -1.6px;
+        color: #000000;
+        font-weight: 700;
+        font-variant: small-caps;
+        text-transform: uppercase;
+    }
+
+
+
 
 </style>
